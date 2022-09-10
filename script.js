235f16e992d394a051008883dacab9471291cf88
Meen Philip <meenphilip@gmail.com>
2022-09-10 13:37:27 +0300

    add toggle and select and change game mode functionality

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const text = document.getElementById("text");
 const scoreEl = document.getElementById("score");
 const timeEl = document.getElementById("time");
 const endgameEl = document.getElementById("end-game-container");
-const settingsBtn = document.getElementById("settings");
-const settings = document.getElementById("settings-form");
+const settingsBtn = document.getElementById("settings-btn");
+const settings = document.getElementById("settings");
+const settingsForm = document.getElementById("settings-form");
 const modeSelect = document.getElementById("mode");
 
 //Array of words for game
@@ -45,6 +46,18 @@ let score = 0;
 
 // init time
 let time = 10;
+
+//set mode in value in LS or else medium
+let mode =
+  localStorage.getItem("mode") !== null
+    ? localStorage.getItem("mode")
+    : "medium";
+
+//set mode select value
+modeSelect.value =
+  localStorage.getItem("mode") !== null
+    ? localStorage.getItem("mode")
+    : "medium";
 
 //Input focus on start
 text.focus();
@@ -97,7 +110,7 @@ function gameOver() {
 
 addWordToDom();
 
-//Event Listeners
+//Typing Event Listeners
 text.addEventListener("input", (e) => {
   const insertedText = e.target.value;
   // console.log(insertedText)
@@ -109,10 +122,30 @@ text.addEventListener("input", (e) => {
     // clear input
     e.target.value = "";
 
-    //add 5 sec upon correct answer 
-    time += 5;
+    if (mode == "hard") {
+      time += 2;
+    } else if (mode == "medium") {
+      time += 3;
+    } else {
+      time += 5;
+    }
+
+    //add 5 sec upon correct answer
+    // time += 5;
 
     //call update time
     updateTime();
   }
+});
+
+// Toggle the mode
+settingsBtn.addEventListener("click", () => settings.classList.toggle("hide"));
+
+//choose mode
+settingsForm.addEventListener("change", (e) => {
+  mode = e.target.value;
+  console.log(mode);
+
+  //store mod in ls
+  localStorage.setItem("mode", mode);
 });
